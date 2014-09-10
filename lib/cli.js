tidev/titanium-cli

commit aaf5cf4a4371aff220e8b02837ad19163e882d71

Fixed bug with missing/invalid field callback referencing non-existent js var. Fixed validated options from being reprocessed.

lib/cli.js
@@ -564,13 +564,13 @@ CLI.prototype.validate = function validate(next) {
 
 						// we asynchronously test each option in order and in series
 						appc.async.series(this, orderedOptionNames.map(function (name) {
-							// if we're prompting, we only need to find the first invalid one
-							if (prompting && invalidCount) return;
-
 							return function (cb) {
+								if (prompting && (missingCount || invalidCount)) return cb();
+								if (options[name].validated) return cb();
+
 								// check missing required options and invalid options
 								var opt = options[name],
-									obj = mix({ name: name }, opt),
+									obj = mix(opt, { name: name }),
 									p = globalOptions.indexOf(name);
 
 								// if this command or platform option is the same name as a global option,
@@ -597,7 +597,6 @@ CLI.prototype.validate = function validate(next) {
 											});
 											return;
 										}
-
 										missing[name] = obj;
 										missingCount++;
 									}
@@ -740,13 +739,15 @@ CLI.prototype.validate = function validate(next) {
 							// do the prompting
 							this.prompt(opt, function (errs) {
 								if (errs) {
-									argv[name] = undefined;
+									argv[opt.name] = undefined;
 								} else {
 									opt._err = null;
+									opt.validated = true;
 									if (opt.callback) {
 										try {
-											var val = opt.callback(argv[name] || '');
-											val !== void 0 && (argv[name] = val);
+											var val = opt.callback(argv[opt.name] || '');
+											val !== void 0 && (argv[opt.name] = val);
+											delete opt.callback;
 										} catch (ex) {
 											if (ex instanceof GracefulShutdown) {
 												// exit the validation and do NOT run the command

commit 9aa61b244821cbc38d5abf949b393a67ff08a81e

[TIMOB-17346] Fixed bug introduced by aaf5cf4a4371aff220e8b02837ad19163e882d71.

lib/cli.js
@@ -681,15 +681,18 @@ CLI.prototype.validate = function validate(next) {
 									Object.keys(invalid).forEach(function (name) {
 										var opt = invalid[name],
 											msg = __('Invalid "%s" value "%s"', (opt.label || '--' + name), argv[opt.name]);
+
 										if (typeof opt.helpNoPrompt === 'function') {
 											opt.helpNoPrompt(this.logger, msg);
-										} else if (opt.values) {
+										} else {
 											this.logger.error(msg + '\n');
-											this.logger.log(__('Accepted values:'));
-											opt.values.forEach(function (v) {
-												this.logger.log('   ' + v.cyan);
-											}, this);
-											this.logger.log();
+											if (opt.values) {
+												this.logger.log(__('Accepted values:'));
+												opt.values.forEach(function (v) {
+													this.logger.log('   ' + v.cyan);
+												}, this);
+												this.logger.log();
+											}
 										}
 									}, this);
 								}